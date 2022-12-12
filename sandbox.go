eddba4be0ce8313b3145b6499ea4f72a07e5f227
yuuki1967 <yuuki1967@github.com>
2022-12-12 09:45:52 +0900

    modify sandobox

--- sandbox.go
+++ sandbox.go
@@ -2,41 +2,33 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path"
-	"runtime"
-	"strings"
-	"time"
+	"io"
 	"log"
-	"github.com/sirupsen/logrus"
+	"net/http"
 )
 
-func ExampleJSONFormatter_CallerPrettyfier() {
-	l := logrus.New()
-	l.SetReportCaller(true)
-	l.Out = os.Stdout
-	l.Formatter = &logrus.JSONFormatter{
-		DisableTimestamp: true,
-		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
-			s := strings.Split(f.Function, ".")
-			funcname := s[len(s)-1]
-			_, filename := path.Split(f.File)
-			return funcname, filename
-		},
-	}
-	l.Info("example of custom format caller")
-	// Output:
-	// {"file":"example_custom_caller_test.go","func":"ExampleJSONFormatter_CallerPrettyfier","level":"info","msg":"example of custom format caller"}
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	defer req.Body.Close()
+	b, _ := io.ReadAll(req.Body)
+	username := string(b)
+	log.Printf("user %s logged in.\n", username)
+	fmt.Fprintf(w, "hello\n")
 }
 
-func handler(ss string) {
-    username := ss
-    log.Printf("user %s logged in.\n", username)
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
 }
 
 func main() {
-	fmt.Println("Welcome to the playground!")
-	handler("test")
-	fmt.Println("The time is", time.Now())
-	ExampleJSONFormatter_CallerPrettyfier()
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":8090", nil)
 }
